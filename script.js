57802d5e8846c02733fd37bd226aa178a4998709
op30mmd <152014555+op30mmd@users.noreply.github.com>
2024-03-20 16:10:30 +0330

    update script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// scripts.js
+
+// Display a welcome message when the page loads
+window.onload = function() {
+    alert('Welcome to the GTA:SA Mods Collection!');
+
+    // Attach click event listeners to all download links
+    var downloadLinks = document.querySelectorAll('a[download]');
+    downloadLinks.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            var modName = event.target.innerText;
+            var confirmDownload = confirm('Do you want to download ' + modName + '?');
+            if (!confirmDownload) {
+                event.preventDefault();
+            }
+        });
+    });
+};
