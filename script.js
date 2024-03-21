21afadba21f87270d72d39f287adf2f223725e38
op30mmd <152014555+op30mmd@users.noreply.github.com>
2024-03-21 12:26:04 +0330

    fixed Download confirmation bug

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
 // scripts.js
 
 // Display a SweetAlert welcome message when the page loads
-window.onload = function() {
+document.addEventListener('DOMContentLoaded', function() {
     swal({
         title: "Welcome!",
         text: "Welcome to the GTA:SA Mods Collection!",
         icon: "success",
         button: "Let's go!",
     });
+});
 
-    // Attach click event listeners to all download links
-    var downloadLinks = document.querySelectorAll('a[download]');
-    downloadLinks.forEach(function(link) {
-        link.addEventListener('click', function(event) {
-            var modName = event.target.innerText;
-            swal({
-                title: "Are you sure?",
-                text: "Do you want to download " + modName + "?",
-                icon: "warning",
-                buttons: true,
-                dangerMode: true,
-            })
-            .then((willDownload) => {
-                if (!willDownload) {
-                    event.preventDefault();
-                }
-            });
-        });
+// Function to handle download confirmation
+function handleDownloadConfirmation(event) {
+    event.preventDefault();
+    var modName = event.target.innerText;
+    var downloadUrl = event.target.parentElement.href;
+
+    swal({
+        title: "Are you sure?",
+        text: "Do you want to download " + modName + "?",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    }).then((willDownload) => {
+        if (willDownload) {
+            window.location.href = downloadUrl;
+        }
     });
-};
+}
+
+// Attach click event listeners to all download links
+document.querySelectorAll('a[download]').forEach(function(link) {
+    link.addEventListener('click', handleDownloadConfirmation);
+});
